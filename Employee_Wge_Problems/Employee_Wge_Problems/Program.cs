3771a0a4263ce332a49c75c7a7bf174ec3e75df0
Amoljamnik97 <ajamnik.mech@gmail.com>
2022-12-01 14:19:08 +0530

    [amol]- Check Employee Is Present or Absent

--- Employee_Wge_Problems/Employee_Wge_Problems/Program.cs
+++ Employee_Wge_Problems/Employee_Wge_Problems/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+    int fullTime = 1;
+    Random randobj = new Random();
+    int checkPresent = randobj.Next(0, 2);
+    Console.WriteLine("Random number=" + checkPresent);
+    if(checkPresent == fullTime)
+    {
+        Console.WriteLine("Employee Is Present");
+    }
+    else
+    {
+        Console.WriteLine("Employee Is Absent");
+    }
+
