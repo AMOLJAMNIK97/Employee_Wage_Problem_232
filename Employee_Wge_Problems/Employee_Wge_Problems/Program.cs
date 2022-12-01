538f61296d04e3c566fab9f97d4c222fb82c15d2
Amoljamnik97 <ajamnik.mech@gmail.com>
2022-12-01 17:17:55 +0530

    [amol]- Calculate Employee Wage

--- Employee_Wge_Problems/Employee_Wge_Problems/Program.cs
+++ Employee_Wge_Problems/Employee_Wge_Problems/Program.cs
@@ -1,15 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-    int fullTime = 1;
+int fullTime = 1, EmpRatePerHour = 20, empHrs = 0, empWage = 0;
     Random randobj = new Random();
     int checkPresent = randobj.Next(0, 2);
     Console.WriteLine("Random number=" + checkPresent);
     if(checkPresent == fullTime)
     {
+        empHrs = 8;
         Console.WriteLine("Employee Is Present");
     }
     else
     {
+        empHrs = 0;
         Console.WriteLine("Employee Is Absent");
     }
-
+    empWage = empHrs * EmpRatePerHour;
+    Console.WriteLine("Employee Wage:" + empWage);
