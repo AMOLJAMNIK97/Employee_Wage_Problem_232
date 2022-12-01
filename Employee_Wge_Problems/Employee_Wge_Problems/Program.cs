faedbad66193299bf20d998881b09de8d3c017a6
Amoljamnik97 <ajamnik.mech@gmail.com>
2022-12-01 22:29:12 +0530

    [amol]- Calculate Employee Wage part Time

--- Employee_Wge_Problems/Employee_Wge_Problems/Program.cs
+++ Employee_Wge_Problems/Employee_Wge_Problems/Program.cs
@@ -1,13 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-int fullTime = 1, EmpRatePerHour = 20, empHrs = 0, empWage = 0;
+int fullTime = 1, PartTime = 2, EmpRatePerHour = 20, empHrs = 0, empWage = 0;
     Random randobj = new Random();
-    int checkPresent = randobj.Next(0, 2);
+    int checkPresent = randobj.Next(0, 3);
     Console.WriteLine("Random number=" + checkPresent);
     if(checkPresent == fullTime)
     {
         empHrs = 8;
         Console.WriteLine("Employee Is Present");
+    }
+    else if(checkPresent == PartTime)
+    {
+        empHrs = 4;
+        Console.WriteLine("Employee is part time");
     }
     else
     {
