5b14f0477489255d1ad85377b51eb3ac2a12a112
Amoljamnik97 <ajamnik.mech@gmail.com>
2022-12-02 11:47:42 +0530

    [amol]- Calculate Daily Wage Assume 20 Day

--- Employee_Wge_Problems/Employee_Wge_Problems/Program.cs
+++ Employee_Wge_Problems/Employee_Wge_Problems/Program.cs
@@ -7,27 +7,34 @@ class Program
     public const int fullTime = 1;
     public const int PartTime = 2;
     public const int EmpRatePerHour = 20;
+    public const int NumOf_Working_day = 20;
      
     static void Main(string[] args)
     {
         int empHrs = 0;
         int empWage = 0;
-        Random randobj = new Random();
-        int checkPresent = randobj.Next(0, 3);
-        Console.WriteLine("Random number=" + checkPresent);
-        switch (checkPresent)
+        int TotalEmpWage = 0;
+        for(int day = 0; day < NumOf_Working_day; day++ )
         {
-            case PartTime:
-                empHrs = 4;
-                break;
-            case fullTime:
-                empHrs = 8;
-                break;
-            default:
-                empHrs = 0;
-                break;
+            Random randobj = new Random();
+            int checkPresent = randobj.Next(0, 3);
+            Console.WriteLine("Random number=" + checkPresent);
+            switch (checkPresent)
+            {
+                case PartTime:
+                    empHrs = 4;
+                    break;
+                case fullTime:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
+            }
+            empWage = empHrs * EmpRatePerHour;
+            TotalEmpWage += empWage;
+            Console.WriteLine("Employee Wage:" + empWage);
         }
-        empWage = empHrs * EmpRatePerHour;
-        Console.WriteLine("Emp Wage :" + empWage);
+        Console.WriteLine("Total Employee Wage :" + TotalEmpWage);
     }
 }
